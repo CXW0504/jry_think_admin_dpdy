97fdf2c5cb79f85ac371f7c77c648581de74ae5e
xiaoyutab <xiaoyutab@qq.com>
2017-09-01 23:33:46 +0800

    完成WAP的登录注册页面

--- Application/Wap/Controller/LoginController.class.php
+++ Application/Wap/Controller/LoginController.class.php
@@ -13,17 +13,59 @@ class LoginController extends \Common\Controller\PublicController{
     /**
      * 构造函数类
      * 
+     * @return void
      * @author xiaoyutab<xiaoyutab@qq.com>
+     * @version v1.0.0
      * @copyright (c) 2017, xiaoyutab
      * @adtime 2017-09-01 16:56:24
      */
     public function __construct() {
+        $this->css(array(
+            'Css/Wap/common','Css/Wap/login',
+        ));
         parent::__construct();
     }
     
+    /**
+     * 注册操作
+     * 
+     * @return void
+     * @author xiaoyutab<xiaoyutab@qq.com>
+     * @version v1.0.0
+     * @copyright (c) 2017, xiaoyutab
+     * @adtime 2017-09-01 23:25:39
+     */
+    public function regAction(){
+        if(!I('post.')){
+            return $this->display();
+        }
+    }
+    
+    /**
+     * 登录操作
+     * 
+     * @return void
+     * @author xiaoyutab<xiaoyutab@qq.com>
+     * @version v1.0.0
+     * @copyright (c) 2017, xiaoyutab
+     * @adtime 2017-09-01 23:07:11
+     */
     public function loginAction(){
         if(!I('post.')){
             return $this->display();
         }
+    }
+
+    /**
+     * 登录欢迎页面
+     * 
+     * @return void
+     * @author xiaoyutab<xiaoyutab@qq.com>
+     * @version v1.0.0
+     * @copyright (c) 2017, xiaoyutab
+     * @adtime 2017-09-01 23:07:07
+     */
+    public function indexAction(){
+        return $this->display();
     }
 }
